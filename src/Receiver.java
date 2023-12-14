058c4e553d78d746305e2341f73832dd45dc0201
JinYao <jnyinwhu@qq.com>
2023-12-14 17:16:13 +0800

    基础功能及握手连接实现（存在bug）

--- src/Receiver.java
+++ src/Receiver.java
@@ -1,23 +1,70 @@
 import java.io.*;
 import java.net.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class Receiver {
-    public static void main(String[] args) {
-        int port = 9876;
-        String savePath = "file/ReceiverFile/11111.md"; // 替换为你想保存文件的路径
+    public static int port = 9876;
+    public static String savePath = "file/ReceiverFile/11111.md"; // 替换为你想保存文件的路径
 
-        try (DatagramSocket socket = new DatagramSocket(port)) {
+    // 建立连接
+
+    // 1-接受连接请求
+    public static DatagramPacket receiveConnectRequest(DatagramSocket socket) throws Exception{
+        byte[] buffer = new byte[1024];
+        DatagramPacket connectPacket = new DatagramPacket(buffer, buffer.length);
+        socket.receive(connectPacket);
+        System.out.println("已收到CONNECT");
+        return connectPacket;
+    }
+
+    // 2-发送连接确认
+    public static void sendConnectConfirm(DatagramSocket socket,InetAddress senderAddress) throws Exception{
+        String acknowledgmentMessage = "ACK";
+        DatagramPacket acknowledgmentPacket = new DatagramPacket(acknowledgmentMessage.getBytes(), acknowledgmentMessage.length(), senderAddress , port);
+        socket.send(acknowledgmentPacket);
+        System.out.println("已发送ACK");
+    }
+
+    // 3-接收对连接确认的确认
+    public static boolean receiveACKConfirmRequest(DatagramSocket socket) throws Exception {
+        byte[] buffer = new byte[1024];
+        DatagramPacket ackConfirmPacket = new DatagramPacket(buffer, buffer.length);
+        socket.receive(ackConfirmPacket);
+        String ackConfirmMessage = new String(ackConfirmPacket.getData(), 0, ackConfirmPacket.getLength());
+        if(ackConfirmMessage.equals("ACK_CONFIRM")){
+            System.out.println("已收到ACK_CONFIRM");
+        }
+        return ackConfirmMessage.equals("ACK_CONFIRM");
+    }
+
+    // 接收文件
+    public static void receiveFile(DatagramSocket socket) throws Exception{
             byte[] buffer = new byte[1024 * 64]; // 根据实际情况调整缓冲区大小
 
-            DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
-            socket.receive(packet);
+            DatagramPacket filePacket = new DatagramPacket(buffer, buffer.length);
+            socket.receive(filePacket);
 
-            try (BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(savePath))) {
-                bos.write(buffer, 0, packet.getLength());
+            try (BufferedOutputStream bos = new BufferedOutputStream(Files.newOutputStream(Paths.get(savePath)))) {
+                bos.write(buffer, 0, filePacket.getLength());
             }
 
-            System.out.println("File received and saved at: " + savePath);
-        } catch (IOException e) {
+            System.out.println("文件接收成功并保存在" + savePath);
+    }
+    public static void main(String[] args) {
+        try (DatagramSocket socket = new DatagramSocket(port)) {
+            // 等待连接请求
+            DatagramPacket connectPacket = receiveConnectRequest(socket);
+
+            // 发送连接确认
+            sendConnectConfirm(socket, connectPacket.getAddress());
+
+            if(receiveACKConfirmRequest(socket)) {
+                // 接收文件
+                receiveFile(socket);
+                System.out.println("文件接收成功并保存在" + savePath);
+            }
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
--- src/Sender.java
+++ src/Sender.java
@@ -3,13 +3,45 @@ import java.net.*;
 import java.nio.file.Files;
 
 public class Sender {
-    public static void main(String[] args) {
-        String filePath = "file/SenderFile/11111.md";
-        String host = "127.0.0.1";
-        int port = 9876;
+    public static String filePath = "file/SenderFile/11111.md";
+    public static String host = "127.0.0.1";
+    public static int port = 9876;
 
-        try (DatagramSocket socket = new DatagramSocket()) {
-            InetAddress receiverAddress = InetAddress.getByName(host);
+    // 建立连接
+
+    // 1-发送连接请求
+    public static void sendConnectRequest(DatagramSocket socket, InetAddress receiverAddress) throws Exception{
+        String connectMessage = "CONNECT";
+        DatagramPacket connectPacket = new DatagramPacket(connectMessage.getBytes(),connectMessage.length(),receiverAddress,port);
+        socket.send(connectPacket);
+        System.out.println("已发送CONNECT");
+    }
+
+    // 2-收到确认消息
+    public static boolean receiveConnectConfirm(DatagramSocket socket) throws Exception{
+        byte[] acknowledgmentBuffer = new byte[1024];
+        DatagramPacket acknowledgmentPacket = new DatagramPacket(acknowledgmentBuffer, acknowledgmentBuffer.length);
+        socket.receive(acknowledgmentPacket);
+
+        String acknowledgmentMessage = new String(acknowledgmentPacket.getData(), 0, acknowledgmentPacket.getLength());
+        if(acknowledgmentMessage.equals("ACK")) {
+            System.out.println("已收到ACK");
+        }
+        return acknowledgmentMessage.equals("ACK");
+    }
+
+    // 3-收到确认消息后发送应答报文，完成三次握手
+    public static boolean sendACKConfirmRequest(DatagramSocket socket, InetAddress receiverAddress) throws Exception{
+        String ackConfirmMessage = "ACK_CONFIRM";
+        DatagramPacket ackConfirmPacket = new DatagramPacket(ackConfirmMessage.getBytes(),ackConfirmMessage.length(),receiverAddress,port);
+        socket.send(ackConfirmPacket);
+        System.out.println("已发送ACK_CONFIRM");
+        return true;
+    }
+
+
+    // 发送文件
+    public static void sendFile(DatagramSocket socket, InetAddress receiverAddress) throws Exception{
             File file = new File(filePath);
             byte[] buffer = new byte[(int) file.length()];
 
@@ -17,11 +49,31 @@ public class Sender {
                 bis.read(buffer, 0, buffer.length);
             }
 
-            DatagramPacket packet = new DatagramPacket(buffer, buffer.length, receiverAddress, port);
-            socket.send(packet);
+            DatagramPacket filePacket = new DatagramPacket(buffer, buffer.length, receiverAddress, port);
+            socket.send(filePacket);
 
-            System.out.println("File sent successfully.");
-        } catch (IOException e) {
+            System.out.println("文件发送成功");
+    }
+
+    public static void main(String[] args) {
+        try(DatagramSocket socket = new DatagramSocket()){
+            InetAddress receiverAddress = InetAddress.getByName(host);
+
+            // 发送连接请求
+            sendConnectRequest(socket, receiverAddress);
+
+            // 接收连接确认
+            if (receiveConnectConfirm(socket)) {
+                // 发送第三次握手确认消息
+                if(sendACKConfirmRequest(socket, receiverAddress)) {
+                    // 发送文件
+                    sendFile(socket, receiverAddress);
+                    System.out.println("文件发送成功");
+                }
+            } else {
+                System.out.println("连接建立失败，已退出");
+            }
+        } catch (Exception e){
             e.printStackTrace();
         }
     }
